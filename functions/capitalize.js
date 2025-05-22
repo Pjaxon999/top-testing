1d858111ab1b0ae8fa9d6e32461107fc2546c893
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-05-22 11:23:39 -0600

    update: added capitalize function and tests

--- functions/capitalize.js
+++ functions/capitalize.js
@@ -1,3 +1,8 @@
 export default function capitalize(string) {
-    return "Meat"
+    if (typeof string !== "string") {return "Strings only please!"}
+    const trimmedString = string.trim();
+    const letterToCapitalize = trimmedString.charAt(0);
+    const capitalizedLetter = letterToCapitalize.toUpperCase();
+    const stringWithoutFirstLetter = trimmedString.slice(1);
+    return capitalizedLetter + stringWithoutFirstLetter;
 }
--- index.test.js
+++ index.test.js
@@ -12,4 +12,28 @@ describe('capitalize function', () => {
     it('capitalizes the first letter of a string', () => {
         expect(capitalize("meat")).toBe("Meat");
     });
+    it('does not just return the input string every time', () => {
+        expect(capitalize("team")).toBe("Team");
+    }); 
+    it('works with strings that have white space', () => {
+        expect(capitalize("   hello world!   ")).toBe("Hello world!");
+    });
+    it('deals with empty strings', () => {
+        expect(capitalize("")).toBe("");
+    });
+    it('does not mess with already capitalized strings', () => {
+        expect(capitalize("Meat")).toBe("Meat");
+    });
+    it('handles bad inputs', () => {
+        expect(capitalize(12)).toBe("Strings only please!");
+    })
+    it('handles strings with non-alphabetic first characters', () => {
+        expect(capitalize("123abc")).toBe("123abc");
+    })
+    it('handles non-english first letters', () => {
+        expect(capitalize("éclair")).toBe("Éclair");
+    })
+    it('handles multi-word strings', () => {
+        expect(capitalize("hello world!")).toBe("Hello world!");
+    })
 });
