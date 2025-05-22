a5d1644e0aef719fd80924537177a98713527d53
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-05-22 10:34:04 -0600

    Update: index and testing file now linked
    The index.js file now serves as the hub for all of the modules that
    will need to be created during the project. The testing file now points
    to index.js and watches it so I can get some real time feedback. I have
    also written an empty test just to confirm that the testing link works
    on it's own.

---
+++ index.js
@@ -0,0 +1,6 @@
+import capitalize from './functions/capitalize';
+import reverseString from './functions/reverseString';
+import calculator from './functions/calculator';
+import caesarCipher from './functions/caesarCipher';
+import analyzeArray from './functions/analyzeArray';
+
